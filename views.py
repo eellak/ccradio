0709d86ce2b5b462e373a1e967037ef670ec8e50
Nikos Roussos <nikos@autoverse.net>
2011-11-19 17:08:28 +0200

    added dynamic track display functionality

--- views.py
+++ views.py
@@ -3,20 +3,22 @@ from django.shortcuts import render_to_response, get_object_or_404, get_list_or_
 from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.conf.urls.defaults import *
 from datetime import date
+from ccradio.panel.models import Broadcaster
 
 
 def get_play(stream):
     import urllib2
     from BeautifulSoup import BeautifulSoup
     html = urllib2.urlopen("http://radio.creativecommons.gr:8000/status.xsl").read()
+    
     soup = BeautifulSoup(html)
-    livetags = soup.findAll('h3')
+    #livetags = soup.findAll('h3')
     
     tdtags = soup.findAll('td', { "class" : "streamdata" })
     try:
-        play = tdtags[-1].contents[0]
+        play = tdtags[int(stream)-1].contents[0]
     except:
-        play = " "
+        play = "radio.creativecommons.gr"
     return play
         
 
@@ -26,7 +28,7 @@ def base(request):
         broadcaster = get_object_or_404(Broadcaster.objects.filter(user=request.user))
         play = get_play(broadcaster.stream)
     else:
-        play = get_play('1')
+        play = get_play('10')
         isonline = False
     return render_to_response('base.html', locals())
 

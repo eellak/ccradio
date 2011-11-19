de8b476b2b2182a3db7715d93495d3cbcb361831
Nikos Roussos <nikos@autoverse.net>
2011-11-19 17:21:01 +0200

    added basic panel functionality

--- panel/views.py
+++ panel/views.py
@@ -4,10 +4,14 @@ from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.conf.urls.defaults import *
 from django.contrib.auth.models import User
 from datetime import date
+from ccradio.panel.models import Broadcaster, Category
+from ccradio.views import get_play
 
 
 def base(request):
     if request.user.is_authenticated():
+        broadcaster = get_object_or_404(Broadcaster.objects.filter(user=request.user))
+        play = get_play(broadcaster.stream)
         return render_to_response('panel.html', locals())
     else:
         return redirect('/')
--- views.py
+++ views.py
@@ -10,7 +10,6 @@ def get_play(stream):
     import urllib2
     from BeautifulSoup import BeautifulSoup
     html = urllib2.urlopen("http://radio.creativecommons.gr:8000/status.xsl").read()
-    
     soup = BeautifulSoup(html)
     #livetags = soup.findAll('h3')
     
@@ -24,18 +23,19 @@ def get_play(stream):
 
 def base(request):
     if request.user.is_authenticated():
-        isonline = True
-        broadcaster = get_object_or_404(Broadcaster.objects.filter(user=request.user))
-        play = get_play(broadcaster.stream)
+        return redirect('/panel/')
     else:
         play = get_play('10')
-        isonline = False
     return render_to_response('base.html', locals())
 
 
 def play(request):
     if request.is_ajax():
-        play = get_play()
+        if request.user.is_authenticated():
+            broadcaster = get_object_or_404(Broadcaster.objects.filter(user=request.user))
+            play = get_play(broadcaster.stream)
+        else:
+            play = get_play('10')
         return render_to_response('play.html', {'play': play})
     else:
         return redirect('/')

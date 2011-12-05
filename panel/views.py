d9a4395345e6ec5e55288eb27ea636b9dad06230
comzeradd <nikos@autoverse.net>
2011-12-05 15:59:10 +0200

    genre pick form ready

--- panel/views.py
+++ panel/views.py
@@ -7,16 +7,18 @@ from datetime import date
 from ccradio.panel.models import Broadcaster, Category, Stream
 from ccradio.views import get_play
 from django.contrib.auth import logout
+from django.db import IntegrityError
 
 
 def base(request):
     if request.user.is_authenticated():
         if request.POST:
             stream = request.POST.get('stream')
-            b = get_object_or_404(Broadcaster.objects.filter(user=request.user))
-            b.stream.id = stream
+            s = Stream.objects.get(id=stream)
+            b = Broadcaster.objects.get(user=request.user)
+            b.stream = s
             b.save()
-        
+            
         broadcaster = get_object_or_404(Broadcaster.objects.filter(user=request.user))
         streams = get_list_or_404(Stream.objects.all())
         play = get_play(broadcaster.stream.uri)

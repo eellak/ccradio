e13fb494a76c4e830b808f99eab70f1a84208ae2
Nikos Roussos <nikos@autoverse.net>
2012-05-17 20:46:23 +0300

    added tos url

--- urls.py
+++ urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     url(r'^play/', views.play),
     url(r'^panel/', pviews.base),
     url(r'^register/', views.register),
+    url(r'^tos/', views.tos),
     url(r'^logout/', pviews.logout_user),
     
     #admin

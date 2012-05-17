068de881bedd8c9823f42834fdae8747f9033e00
Nikos Roussos <nikos@autoverse.net>
2012-05-17 18:51:19 +0300

    added ip

--- panel/views.py
+++ panel/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response, get_object_or_404, get_list_or_404, redirect
-from django.http import HttpResponseRedirect, Http404, HttpResponse
+from django.http import HttpResponseRedirect, Http404, HttpResponse, HttpRequest
 from django.conf.urls.defaults import *
 from django.contrib.auth.models import User
 from datetime import date
@@ -19,13 +19,16 @@ def base(request):
             b = Broadcaster.objects.get(user=request.user)
             b.stream = s
             b.save()
+            """
             stime = strftime("%H:%M:%S")
             GenresLog.date = stime
             GenresLog.broadcaster = b
             GenresLog.stream = s
             GenresLog.save()
+            """
             
         broadcaster = get_object_or_404(Broadcaster.objects.filter(user=request.user))
+        ip = request.META.get('HTTP_X_FORWARDED_FOR', '')
         streams = get_list_or_404(Stream.objects.all())
         play = get_play(broadcaster.stream.uri)
         return render_to_response('panel.html', locals())

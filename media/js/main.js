32dde108bc9d98a0052127027096e032195c857d
comzeradd <nikos@autoverse.net>
2012-05-18 14:39:22 +0300

    added tos functionality

--- media/js/main.js
+++ media/js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
     var refreshId = setInterval(function() {
         $('#live_play').load('/play/');
     }, 100000);
+    
+    $('a#tosopen').click(function() {
+        $('#tos').show('slow');
+    });
+
+    $('a#tosexit').click(function() {
+        $('#tos').hide('slow');
+    });
 });

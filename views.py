5b839495b017e61bcedea460d376940c4bd3dda7
Nikos Roussos <nikos@autoverse.net>
2012-06-07 21:16:21 +0300

    remove custom registration view

--- views.py
+++ views.py
@@ -87,3 +87,22 @@ def play(request):
         return render_to_response('play.html', {'play': play})
     else:
         return redirect('/')
+        
+        
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+def register2(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        data = request.POST.copy()
+        errors = form.get_validation_errors(data)
+        if not errors:
+            new_user = form.save()
+            return HttpResponseRedirect("/accounts/created/")
+    else:
+        data, errors = {}, {}
+
+    return render_to_response("registration/register.html", {
+        'form' : forms.FormWrapper(form, data, errors)
+    })

8862a039eb4fa6cc8afc3f7d9dc7226467ca3dc6
comzeradd <nikos@roussos.cc>
2012-10-08 13:36:17 +0300

    remove not needed function

--- media/js/main.js
+++ media/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var refreshId = setInterval(function() {
         $('#live_play').load('/play/');
     }, 10000);
-    
+
     $('a#tosopen').click(function() {
         $('#tos').show('slow');
     });
@@ -10,7 +10,7 @@ $(document).ready(function() {
     $('a#tosexit').click(function() {
         $('#tos').hide('slow');
     });
-    
+
     $('a#aboutopen').click(function() {
         $('#about').show('slow');
     });
@@ -18,12 +18,7 @@ $(document).ready(function() {
     $('a#aboutexit').click(function() {
         $('#about').hide('slow');
     });
-    
+
     $("#datepicker").datepicker({ dateFormat: "yyMdd" });
-    
-    $("button#getlogs").click(function() {
-        var query = $("input#datepicker").val();
-        $.get("http://ccradiologs.ellak.gr/playlist-"+query+"txt.zip");
-    });
 });
 
